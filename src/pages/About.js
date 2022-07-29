8516f20bbc799cc7ec2b506d747c6f57dabc6bd7
KELVINMBAKA <loverc832@gmail.com>
2022-07-29 18:29:23 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'> ABOUT US</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        corporis labore recusandae molestias voluptas vel provident veniam
+        dolorem quis pariatur, impedit tenetur cumque alias! Unde eligendi
+        accusamus asperiores quidem quibusdam?
+      </p>
+    </section>
+  )
+}
+
+export default About
