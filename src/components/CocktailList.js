988d960c84903a5091cc3e07cdefb7d671fc4a6d
KELVINMBAKA <loverc832@gmail.com>
2022-07-29 20:42:55 +0300

    Added a loader

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,14 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (cocktails.length < 1) {
+    return <h2> no coctails matched your serach</h2>
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
